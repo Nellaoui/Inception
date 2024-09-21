9c105966bdae21240643eec4fe52e49b10d8c4bc
Your Name <your-email@example.com>
2024-09-20 20:27:31 -0400

    skip first configuration

--- srcs/wordpress/conf/config.sh
+++ srcs/wordpress/conf/config.sh
@@ -4,7 +4,7 @@ sleep 5
 #     {
         wp core download --allow-root --path=/var/www/wordpress/
         mv /wp-config.php /var/www/wordpress/
-        wp core install   --url=$(DOMAIN_NAME)  --title=$(TITLE) --admin_user=$(ADMIN_USER) --admin_password=$(ADMIN_PASSWORD) --admin_email=$(ADMIN_EMAIL) --skip-email --allow-root --path=/var/www/wordpress/ 
+        wp core install   --url=${DOMAIN_NAME}  --title=${TITLE} --admin_user=${ADMIN_USER} --admin_password=${ADMIN_PASSWORD} --admin_email=${ADMIN_EMAIL} --skip-email --allow-root --path=/var/www/wordpress/ 
 
         # wp user create $WP_USER $WP_EMAIL --role=author --user_pass=$WP_PASSWD --allow-root
         echo "wordpress installes succes"
